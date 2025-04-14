7ceb890545e08ccef00abf1dfcc9a2eaecdafe9d
Vitor Luiz De Carvalho Gonçalves <vitorluizcarvalho@professor.educacao.sp.gov.br>
2025-04-14 12:02:16 -0300

    post produtos

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express')
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const fs = require("fs");
 const { parse } = require("csv-parse");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const app = express()
+
+app.use(express.json())
+
 const port = 3000
+
+const csvWriter = createCsvWriter({
+  path: "./produtos.csv",
+  header: [
+    {id: "nomeProduto", title: "nomeProduto"},
+    {id: "valor", title: "valor"}
+  ]
+})
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', (req, res) => {
+app.get('/produtos', (req, res) => {
   fs.readFile("./produtos.csv", (err, data) => {
     if (err) {
       res.status(500).send("Error reading file")
@@ -23,6 +36,23 @@ app.get('/', (req, res) => {
       res.send(records)
     })
   })
+})
+
+app.post("/produtos", (req, res) => {
+  // pegar as informações (nomeProduto e valor) da requisição
+  const {nomeProduto, valor} = req.body;
+  // acessar o arquivo csv e modifica-lo
+  fs.readFile("./produtos.csv", (err, data) => {
+    parse(data, {columns: true, trim: true}, (err, records) => {
+      records.push({nomeProduto, valor})
+      csvWriter.writeRecords(records).then(() => {
+        console.log("Feito")
+      })
+    })
+  })
+  // retornar a resposta para o cliente (201 Ok)
+  res.status(201);
+  res.send("Produto cadastrado com sucesso!");
 })
 
 app.listen(port, () => {
